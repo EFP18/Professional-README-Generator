3fe06eac825431c11db0aed75e1011b20a70982e
EFP18 <ester.p18@hotmail.com>
2023-04-16 07:59:35 -0500

    changed double quotation marks for nodejs best practices

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [{
-  type: "input",
-  name: "Project Title",
-  message: "What is the title of your README file?", 
+  type: 'input',
+  name: 'Project Title',
+  message: 'What is the title of your README file?', 
+  default: 'Project Title',
 },
 {
-  type: "input",
-  name: "Description",
-  message: "Write a description for your repository.", 
+  type: 'input',
+  name: 'Description',
+  message: 'Write a description for your repository.', 
+  default: 'Description',
 },
 {
-  type: "input",
-  name: "Table of Contents",
-  message: "Create a Table of Contents.", 
+  type: 'input',
+  name: 'Table of Contents',
+  message: 'Create a Table of Contents.', 
+  default: 'Table of Contents',
 },
 {
-  type: "input",
-  name: "Installation",
-  message: "What are the installation instructions for your project?", 
+  type: 'input',
+  name: 'Installation',
+  message: 'What are the installation instructions for your project?',
+  default: 'Installation', 
 },
 {
-  type: "input",
-  name: "Usage",
-  message: "What's the usage of your repository?", 
+  type: 'input',
+  name: 'Usage',
+  message: 'What is the usage of your repository?', 
+  default: 'Usage',
 },
 {
-  type: "list",
-  name: "License",
-  message: "What license(s) does your repository have?", 
-  choices: ['MIT', 'Microsoft Public License', 'Apache License 2.0'],
+  type: 'list',
+  name: 'License',
+  message: 'What license(s) does your repository have?', 
+  choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+  default: 'License',
   // explanation for each license??
   // list of licenses
   // license badge at the top
   // do I need all license types?
 },
 {
-  type: "input",
-  name: "Contribution",
-  message: "Who contributed to your project?", 
+  type: 'input',
+  name: 'Contributing',
+  message: 'How can someone contribute to your repository?', 
+  default: 'Contributing',
 },
 {
-  type: "input",
-  name: "Tests",
-  message: "???", 
+  type: 'input',
+  name: 'Tests',
+  message: '???', 
+  default: 'Tests',
 },
 {
-  type: "input",
-  name: "Questions",
-  message: "What is your GitHub profile?", 
-  message: "What is your email address?", 
-
+  type: 'input',
+  name: 'Questions',
+  message: 'What is your GitHub profile?', 
+  message: 'What is your email address?', 
+  default: 'Questions',
+// how to add multiple questions 
 }];
 
 // TODO: Create a function to write README file
@@ -65,8 +74,8 @@ const askUserAndCreateReadme = async () => {
     const data = await inquirer.prompt(questions);
     console.log(data);
 
-    await fs.promises.writeFile("README-autoCreated.md", JSON.stringify(data, null, '\t'));
-    console.log("You have a README file!");
+    await fs.promises.writeFile('README-autoCreated.md', JSON.stringify(data, null, '\t'));
+    console.log('You have a README file!');
 
   } catch (err) {
     console.log(err);
